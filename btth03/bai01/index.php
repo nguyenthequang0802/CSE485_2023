498dfbbc49e1a510026ed0f350fb4222cc676a40
nguyenthequang0802 <quangnguyenthe7@gmail.com>
2023-12-19 00:07:30 +0700

    upload cms_posts

--- btth03/bai01/index.php
+++ btth03/bai01/index.php
@@ -2,3 +2,30 @@
 $controller = isset($_GET['controller']) ? $_GET['controller'] : 'home';
 $action = isset($_GET['action']) ? $_GET['action']: 'index';
 
+if ($controller == 'post'){
+    require_once './controllers/PostController.php';
+    $postController = new PostController();
+    switch ($action){
+        case 'index':
+            $postController->index();
+            break;
+        case 'create':
+            $postController->create();
+            break;
+        case 'store':
+            $postController->store();
+            break;
+        case 'edit':
+            $postController->edit();
+            break;
+        case 'update':
+            $postController->update();
+            break;
+        case 'delete':
+            $postController->delete();
+            break;
+        default:
+            $postController->index();
+            break;
+    }
+}
